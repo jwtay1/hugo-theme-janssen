f824ea32df91a3e0028bbaa475b3fab5f54b3195
Jian Tay <jian.tay@colorado.edu>
2025-04-07 14:12:34 -0600

    Fixed sidebar and added light/dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,69 @@ const menuToggler = document.getElementById("menuToggler");
 const html = document.documentElement;
 var isNavOpen = false;
 
+/**
+ * Closes the sidebar menu if it is open
+ * @param {*} ev 
+ */
 function handleClicks(ev) {
-    //Close the sidebar menu if it's open and remove the event listener
     ev.stopPropagation();
-
-    console.log(ev.target.id);
 
     if (ev.target.id !== "navmenu") {
         closeNav();
     }
 }
 
+/**
+ * Open the sidebar menu by appending the 'show' class
+ */
 function openNav() {
-    console.log("Open")
-    document.getElementById("navmenu").style.display = "block";
+    menu = document.getElementById('navmenu');
 
+    if (!menu.classList.contains('show')) {
+        console.log("Adding show");
+        menu.classList.add('show');
+    }
+    
     html.addEventListener("click", handleClicks, true);
 
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/**
+ * Closes the sidebar menu by removing the 'show' class
+ */
 function closeNav() {
 
         console.log("Close")
-        document.getElementById("navmenu").style.display = "none";
+
+        menu = document.getElementById('navmenu');
+
+        if (menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        }
+
         html.removeEventListener("click", handleClicks, true);
 
 }
 
 menuToggler.onclick = openNav;
+
+//Theme toggler
+
+const toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
