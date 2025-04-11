de1db730ccd423add085b19ae98e442b7338eae2
Jian Wei Tay <jian.tay@colorado.edu>
2025-04-10 18:41:02 -0600

    Tidying up

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ window.onload = function () {
      */
     function handleClicks(ev) {
 
-        if (ev.target.id !== "navmenu" && !(ev.target.classList.contains("btn-dropdown-icon") || ev.target.classList.contains("dd-icon"))) {
+        if (ev.target.id !== "navmenu" && !(ev.target.classList.contains("sidebar-btn-icon") || ev.target.classList.contains("dd-icon"))) {
             closeNav();
             ev.stopPropagation();
         }
@@ -99,21 +99,7 @@ window.onload = function () {
 
     //Sidebar item toggler
     window.unfold = function unfold(obj) {
-
-        obj.parentElement.parentElement.classList.toggle("unfold-sidebar");
-
-        
-        /*var lists = obj.parentElement.parentElement.getElementsByTagName('ul');
-        lists[0].classList.toggle("unfold-sidebar");
-
-        var svgs = obj.parentElement.getElementsByTagName('svg');
-        if (lists[0].classList.contains("unfold-sidebar")) {            
-            svgs[0].style.transform = 'rotate(90deg)';
-        }
-        else {
-            svgs[0].style.transform = '';
-        }
-            */
+        obj.parentElement.parentElement.classList.toggle("unfold-sidebar");        
     }
 
     //Theme toggler
@@ -132,11 +118,9 @@ window.onload = function () {
 
     codeBlocks.forEach(codeBlock => {
         let button = document.createElement('button');
-        button.className = 'copy-button';
-        button.innerText = 'Copy';
-
-        codeBlock.parentNode.style.position = 'relative';
-        button.style.position = 'absolute';
+        button.className = 'code-copy-button';
+        //button.innerText = 'Copy';
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
 
         codeBlock.parentNode.appendChild(button);
 
@@ -145,7 +129,7 @@ window.onload = function () {
                 .then(() => {
                     button.innerText = 'Copied!';
                     setTimeout(() => {
-                        button.innerText = 'Copy';
+                        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
                     }, 2000);
                 })
                 .catch(err => {
