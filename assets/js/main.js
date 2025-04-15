93e238d6f700e118fea45de31e1655c2a08a44ad
Jian Wei Tay <jian.tay@colorado.edu>
2025-04-15 17:06:19 -0600

    Refactor and tify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
-//Initialize the current page theme
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+/**
+ * Site themer
+ * 
+ * This function needs to run before the page loads to avoid 'flickering'.
+ * The code uses the localStorage to store theme preference.
+ */
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+//Initialize the site theme
+var currentTheme = null;
+
+if (localStorage.getItem('theme') === null) {
+    //If theme is not set, set it to 'light'
+    currentTheme = (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+}
+else {
+    currentTheme = localStorage.getItem('theme');
 }
 
-else {
-
-    //See if there's a preferred theme. If not, default to light.
-    var preferredTheme = (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-
-    document.documentElement.setAttribute('data-theme', preferredTheme);
+//Set the data-theme of the page
+if (currentTheme) {    
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    localStorage.setItem('theme', currentTheme);
 }
 
 function switchTheme(e) {
@@ -26,111 +35,99 @@ function switchTheme(e) {
 
 }
 
-//These run after the page has loaded
-window.onload = function () {
+//Scrollspy for the table of contents
+window.addEventListener('DOMContentLoaded', () => {
 
-    const menuToggler = document.getElementById("menuToggler");
-    const html = document.documentElement;
-    var isNavOpen = false;
+    //Attach a listener to the theme toggler checkbox
+    const themetoggle = document.getElementById('themetoggle');
 
-    /**
-     * Closes the sidebar menu if it is open
-     * @param {*} ev 
-     */
-    function handleClicks(ev) {
+    if (themetoggle !== null) {
 
-        if (ev.target.id !== "navmenu" && !(ev.target.classList.contains("sidebar-btn-icon") || ev.target.classList.contains("dd-icon"))) {
-            closeNav();
-            ev.stopPropagation();
-        }
-    }
-
-    /**
-     * Open the sidebar menu by appending the 'show' class
-     */
-    function openNav() {
-        menu = document.getElementById('navmenu');
-
-        if (menu.classList.contains('hide')) {
-            menu.classList.remove('hide');
-        }
-
-        if (!menu.classList.contains('show')) {
-            menu.classList.add('show');
-        }
-
-        html.addEventListener("click", handleClicks, true);
-
-    }
-
-    /**
-     * Closes the sidebar menu by removing the 'show' class
-     */
-    function closeNav() {
-        menu = document.getElementById('navmenu');
-
-        if (menu.classList.contains('show')) {
-            menu.classList.remove('show');
-            menu.classList.add('hide');
-            menu.addEventListener("animationend", function () {
-                menu.classList.remove('hide');
-            })
-        }
-
-        html.removeEventListener("click", handleClicks, true);
-
-    }
-
-    menuToggler.onclick = openNav;
-
-    //Table of contents toggler
-    window.showTOC = function showTOC() {
-        var toc = document.getElementById("toc");
-        toc.classList.toggle("unfold-toc");
-
-        var dropbtn = toc.parentElement.getElementsByTagName("button");
-        if (toc.classList.contains("unfold-toc")) {                        
-            dropbtn[0].style.borderRadius = "10px 10px 0 0";
+        if (localStorage.getItem('theme') === 'light') {
+            themetoggle.checked = true;
         }
         else {
-            dropbtn[0].style.borderRadius = "10px";
+            themetoggle.checked = false;
+        }
+
+        themetoggle.addEventListener("change", switchTheme, true);
+    }
+
+    //Styling the links of table of contents
+    const toc = document.getElementById("TableOfContents");
+
+    if (toc !== null) {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const id = entry.target.getAttribute('id');
+                if (entry.intersectionRatio > 0) {
+                    document.querySelector(`nav li a[href="#${id}"]`).classList.add('active');
+                } else {
+                    document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active');
+                }
+            });
+        });
+
+        // Track all headings that have an `id` applied
+        document.querySelectorAll('h2[id], h3[id], h4[id]').forEach((heading) => {
+            observer.observe(heading);
+        });
+
+    }
+
+});
+
+window.onload = () => {
+
+    window.toggleSidebar = function toggleSidebar() {
+
+        //Get sidebar object
+        var sidebar = document.getElementById("sidebar");
+
+        if (sidebar !== null) {
+            sidebar.classList.toggle('show');
+
+            if (sidebar.classList.contains('show')) {
+                window.addEventListener("click", handleClick, true);
+            }
+            else {
+                window.removeEventListener("click", handleClick, true);
+            }
         }
     }
 
-    //Sidebar item toggler
+    function handleClick(event) {
+
+        if (event.target.classList.contains("sidebar")) {
+            toggleSidebar();
+        }
+    }
+
     window.unfold = function unfold(obj) {
-        obj.parentElement.parentElement.classList.toggle("unfold-sidebar");        
+
+        obj.parentElement.classList.toggle("unfold");
+
     }
 
-    //Theme toggler
-    const toggle = document.getElementById("theme-toggle");
-
-    //Set initial value
-    if (currentTheme === 'light') {
-        toggle.checked = true;
-    }
-
-    //Add a listener to the theme switcher
-    toggle.addEventListener('change', switchTheme, true);
-
-    //Add copy button for code blocks - thanks to Bard!
+    //Add a copy button to code blocks
     let codeBlocks = document.querySelectorAll('pre code');
+    console.log(codeBlocks);
 
     codeBlocks.forEach(codeBlock => {
         let button = document.createElement('button');
         button.className = 'code-copy-button';
         //button.innerText = 'Copy';
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
 
         codeBlock.parentNode.appendChild(button);
 
         button.addEventListener('click', () => {
             navigator.clipboard.writeText(codeBlock.innerText)
                 .then(() => {
-                    button.innerText = 'Copied!';
+                    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#000000"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>';
                     setTimeout(() => {
-                        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
-                    }, 2000);
+                        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
+                    }, 1000);
                 })
                 .catch(err => {
                     console.error('Failed to copy text: ', err);
@@ -138,4 +135,5 @@ window.onload = function () {
                 });
         });
     });
-};
+
+}
